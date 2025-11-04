77d4d6ed9c8c70fa3ad957ed4da5ff24f9d433c2
Salbila <salbiladj8@gmail.com>
2025-11-04 17:16:30 +0700

    Modifikasi Percobaan 2

--- joobsheet5/switchCetakKRS28.java
+++ joobsheet5/switchCetakKRS28.java
@@ -6,34 +6,24 @@ public class switchCetakKRS28 {
         System.out.println("--- Cetak KRS SIAKAD ----");
         System.out.println("Masukan semester saat ini: ");
         int semester = sc.nextInt();
-        switch (semester) {
-    case 1:
+        if (semester == 1) {
             System.out.println("KRS semester 1 ditampilkan");
-            break;
-    case 2:
+        } else if (semester == 2) {
             System.out.println("KRS semester 2 ditampilkan");
-            break;
-    case 3:
+        } else if (semester == 3) {
             System.out.println("KRS semester 3 ditampilkan");
-            break;
-    case 4:
+        } else if (semester == 4) {
             System.out.println("KRS semester 4 ditampilkan");
-            break;
-    case 5:
+        } else if (semester == 5) {
             System.out.println("KRS semester 5 ditampilkan");
-            break;
-    case 6:
+        } else if (semester == 6) {
             System.out.println("KRS semester 6 ditampilkan");
-            break;
-    case 7:
+        } else if (semester == 7) {
             System.out.println("KRS semester 7 ditampilkan");
-            break;
-    case 8:
+        } else if (semester == 8) {
             System.out.println("KRS semester 8 ditampilkan");
-            break;
-    default:
+        }  else {
             System.out.println("Semester tidak valid");
-            break;
         } 
     }         
 }
