31a13185bbc26f943e92b823775ebf25a962aec3
Salbila <salbiladj8@gmail.com>
2025-11-04 17:41:19 +0700

    Tugas 1

---
+++ joobsheet5/ifCetak28.java
@@ -0,0 +1,12 @@
+package joobsheet5;
+import java.util.Scanner;
+public class ifCetak28 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("--- Cetak KRS SIAKAD ----");
+        System.out.println("Apakah UKT sudah Lunas? (true/false):");
+        boolean uktLunas = sc.nextBoolean();
+        String x = (uktLunas) ? "Pembayaran UKT terverifikasi" :  "Registrasi ditolak, silahkan lunasi UKT terlebih dahulu";
+        System.out.println(x);   
+        } 
+}
