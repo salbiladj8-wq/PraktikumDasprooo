e59c060d0f90ae86c0f66167031653274303d1d6
Salbila <salbiladj8@gmail.com>
2025-11-04 18:37:05 +0700

    tugas 1

---
+++ joobsheet7/siakadFor28.java
@@ -0,0 +1,22 @@
+package joobsheet7;
+import java.util.Scanner;
+public class siakadFor28 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        double tertinggi = 0;
+        double terendah = 100;
+        for (int i = 1; i <= 10; i++) {
+            System.out.print("Masukkan nilai mahasiswa ke-" + i + ": ");
+            double nilai = scanner.nextDouble();
+
+            if (nilai > tertinggi) {
+                tertinggi = nilai;
+            }
+            if (nilai < terendah) {
+                terendah = nilai;
+            }
+        }
+        System.out.println("Nilai tertinggi: " + tertinggi);
+        System.out.println("Nilai terendah: " + terendah);
+    }
+}
