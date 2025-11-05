b2653892a5715e85457cdb7dcde0823156539f1b
Salbila <salbiladj8@gmail.com>
2025-11-05 11:31:55 +0700

    Modifikasi 2 Percobaan 2

--- joobsheet9/ArrayNilai28.java
+++ joobsheet9/ArrayNilai28.java
@@ -4,11 +4,12 @@ public class ArrayNilai28 {
     public static void main(String[] args) {
         Scanner sc = new Scanner (System.in);
         int [] nilaiAkhir = new int [10];
-        for (int i = 0; i < nilaiAkhir.length; i++){
+        for (int i = 0; i < 10; i++){
             System.out.print("Masukkan nilai akhir mahasiswa ke-" + i + ": ");
             nilaiAkhir[i] = sc.nextInt();
-        }for (int i = 0; i < 10; i++){
-            System.out.println("Nilai akhir ke-" + i + " adalah: " + nilaiAkhir[i]);
+        }for (int i = 0; i < nilaiAkhir.length; i++){
+            if (nilaiAkhir [i] > 70)
+            System.out.println("Nilai akhir ke-" + i + " lulus! ");
 
         } 
     }
