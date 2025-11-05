8c3e6974b81046f0b95699eac19b38af1a7f135e
Salbila <salbiladj8@gmail.com>
2025-11-05 11:23:29 +0700

    Modifikasi 1 Percobaan 2

--- joobsheet9/ArrayNilai28.java
+++ joobsheet9/ArrayNilai28.java
@@ -4,7 +4,7 @@ public class ArrayNilai28 {
     public static void main(String[] args) {
         Scanner sc = new Scanner (System.in);
         int [] nilaiAkhir = new int [10];
-        for (int i = 0; i < 10; i++){
+        for (int i = 0; i < nilaiAkhir.length; i++){
             System.out.print("Masukkan nilai akhir mahasiswa ke-" + i + ": ");
             nilaiAkhir[i] = sc.nextInt();
         }for (int i = 0; i < 10; i++){
