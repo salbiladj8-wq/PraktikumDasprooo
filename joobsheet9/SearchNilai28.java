ced1c7fb96a625c20e0ccaa05e1798abc57be49f
Salbila <salbiladj8@gmail.com>
2025-11-05 13:03:10 +0700

    Modifikasi 1 asli Percobaan 4

--- joobsheet9/SearchNilai28.java
+++ joobsheet9/SearchNilai28.java
@@ -3,16 +3,16 @@ import java.util.Scanner;
 public class SearchNilai28 {
     public static void main (String[] args) {
         Scanner sc = new Scanner (System.in);
-        System.out.print("Masukkan jumlah elemen array: ");
-        int jmlElmn = sc.nextInt();
-        int [] arrNilai = new int [jmlElmn];
-        for (int i = 0; i < jmlElmn; i++) {
-            System.out.print("Masukkan nilai mahasiswa ke-" + i + ": ");
+        System.out.print("Masukkan banyaknya nilai yang akan diinput: ");
+        int banyakNilai = sc.nextInt();
+        int [] arrNilai = new int [banyakNilai];
+        for (int i = 0; i < banyakNilai; i++) {
+            System.out.print("Masukkan nilai mahasiswa ke-" + (i + 1) + ": ");
             arrNilai[i] = sc.nextInt();
         }
         System.out.print("Masukkan nilai yang dicari: ");
         int key = sc.nextInt();
-        int hasil = -1;
+        int hasil = 0;
         for (int i = 0; i < arrNilai.length; i++) {
             if (key == arrNilai [i]) {
                 hasil = i;
@@ -20,10 +20,8 @@ public class SearchNilai28 {
             }
         }
         System.out.println ();
-        if (hasil != -1) {
-            System.out.println("Nilai " + key + " Ketemu di indeks ke-" + hasil + ".");
-        } else {
-            System.out.println("Nilai " + key + "  Tidak ditemukan dalam array.");
-        }
+        if (hasil != 0) {
+            System.out.println("Nilai " + key + " Ketemu, merupakan nilai mahasiswa ke-" + (hasil + 1) + ".");
     }    
+}
 }
