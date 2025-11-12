14536f7c521e99ddc605f54770f2668ee393dfa9
Salbila <salbiladj8@gmail.com>
2025-11-12 10:55:32 +0700

    Percobaan 1

---
+++ joobsheet11/Bioskop28.java
@@ -0,0 +1,14 @@
+package joobsheet11;
+public class Bioskop28 {
+     public static void main(String[] args) {
+        String [] [] penonton = new String [4] [2];
+        penonton [0] [0] = "Amin";
+        penonton [0] [1] = "Bena";
+        penonton [1] [0] = "Candra";
+        penonton [1] [1] = "Dela";
+        penonton [2] [0] = "Eka";
+        penonton [2] [1] = "Farhan";
+        penonton [3] [0] = "Gisel";
+        penonton [3] [1] = "Hana";
+    }
+}
